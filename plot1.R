0e17c6de006519af4aa33c970513653b7222b11b
mabdelaziz <mohamed.abdelaziz_82@yahoo.com>
2015-04-13 04:58:15 +0200

    Project assignment 1 , 4 plots refine plots

--- plot1.R
+++ plot1.R
@@ -39,22 +39,5 @@ return(RR)
 ###########################################################################################################
 
 
-# Read dataset 
-power <- read.csv(filen, header=T, sep=';', na.strings="?") 
-power$Date <- as.Date(power$Date, format="%d/%m/%Y")                       
-
-
-# Subset the data to include only those between 01 Feb 2007 and 02 Feb 2007 
-power_ss <- subset(power, subset=(Date >= "2007-02-01" & Date <= "2007-02-02")) 
-
-
-# Create a histogram of Global Active Power 
-hist(power_ss$Global_active_power, main="Global Active Power",      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red") 
-
-
-# Save graph 
-dev.copy(png, file="plot1.png", height=480, width=480) 
-dev.off() 
-
 
 
